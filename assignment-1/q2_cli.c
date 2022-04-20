b21cb1b74f8926ab2293e63086296174cd98dd0b
SAVSANI SEJAL <71888563+sejalsavasani-1914@users.noreply.github.com>
2022-04-20 11:22:48 +0530

    Add files via upload

---
+++ assignment-1/q2_cli.c
@@ -0,0 +1,39 @@
+#include<stdio.h>
+#include<unistd.h>
+#include<sys/socket.h>
+#include<sys/types.h>
+#include<string.h>
+#include<netinet/in.h>
+#include<stdlib.h>
+
+int main()
+{
+    char buff[200];
+    int n;
+    char *serv_ip = "127.0.0.1";
+    int sockfd,ret_val;
+    struct sockaddr_in servaddr;
+    sockfd = socket(AF_INET, SOCK_STREAM, 0);
+
+    bzero(&servaddr, sizeof(servaddr));
+    servaddr.sin_family = AF_INET;
+    servaddr.sin_port = htons(8000);
+    inet_pton(AF_INET, serv_ip, &servaddr.sin_addr);
+
+    ret_val = connect(sockfd, (struct sockaddr *) &servaddr, sizeof(servaddr));
+
+    if(ret_val < 0)
+    {
+        perror("connect:");
+        exit(1);
+    }
+    printf("enter the data that you want to send to the server\n");
+    gets(buff);
+    write(sockfd,buff,strlen(buff));
+    n = read(sockfd,buff,200);
+    buff[n]='\0';
+    printf("rec'd %s from server\n",buff);
+    getchar();
+    close(sockfd);
+    return 0;
+}
---
+++ assignment-1/q2_srv.c
@@ -0,0 +1,65 @@
+#include<stdio.h>
+#include<unistd.h>
+#include<sys/socket.h>
+#include<sys/types.h>
+#include<string.h>
+#include<netinet/in.h>
+#include<stdlib.h>
+
+void handleclient(int connfd)
+{
+    int n;
+    char buff[200];
+    n = read(connfd,buff,200);
+    buff[n] = '\0';
+    printf("data rec'd from client = %s\n",buff);
+    printf("requested handled by server %d\n",getpid());
+    write(connfd,"good bye",8);
+    exit(0);
+}
+
+int main()
+{
+    int listfd,connfd,retval;
+    pid_t childpid;
+    socklen_t clilen;
+    struct sockaddr_in cliaddr, servaddr;
+
+    listfd = socket(AF_INET, SOCK_STREAM,0);
+    if(listfd < 0)
+    {
+        perror("sock:");
+        exit(1);
+    }
+    bzero(&servaddr,sizeof(servaddr));
+    servaddr.sin_family = AF_INET;
+    servaddr.sin_addr.s_addr = htonl(INADDR_ANY);
+    servaddr.sin_port = htons(8000);
+
+    retval = bind(listfd,(struct sockaddr *) &servaddr,sizeof(servaddr));
+    if(retval < 0)
+    {
+        perror("bind:");
+        exit(2);
+    }
+
+    listen(listfd, 5);
+
+    while(1)
+    {
+        char buff[200];
+        pid_t p;
+        int n;
+        clilen = sizeof(cliaddr);
+        connfd = accept(listfd, (struct sockaddr *) &cliaddr,clilen);
+        printf("client connected\n");
+        p = fork();
+        if(p==0)
+        {
+            close(listfd);
+            handleclient(connfd);
+        }
+        close(connfd);
+    }
+    return 0;
+}
